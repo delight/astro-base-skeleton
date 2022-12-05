171674bdf6852bd6b25545e156ebd0850a51c649
Konstantinos Kostarellis <dev@kostarellis.com>
2022-12-05 21:31:37 +0100

    Wrapper fix for Breadcrump Component

---
+++ src/components/WrapperBreadCrump.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { Breadcrumb, Crumb } from "@skeletonlabs/skeleton";
+    const customSeparator: string = `<span class="text-surface-500">/</span>`;
+</script>
+
+<Breadcrumb separator={customSeparator}>
+    <Crumb href="/">Home</Crumb>
+    <Crumb>About</Crumb>
+</Breadcrumb>
